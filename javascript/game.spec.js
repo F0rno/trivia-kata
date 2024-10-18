1cfb7e39b5807b4e0ee1c6588f7c3b18eb7e7933
F0rno <pablo.fornell.contacto@gmail.com>
2024-10-18 19:08:54 +0200

    Golden Master

--- javascript/game.spec.js
+++ javascript/game.spec.js
@@ -1,4 +1,7 @@
 require('./game.js');
+const { execSync } = require('child_process');
+const fs = require('fs');
+
 
 describe("The test environment", function() {
   it("should pass", function() {
@@ -10,6 +13,12 @@ describe("The test environment", function() {
   });
 });
 
-describe("Your specs...", function() {
-  // it ...
+describe("Golden Master", function() {
+  it("should be", function() {
+    execSync('node game.js > result1')
+    execSync('node game.js > result2')
+    const result1 = fs.readFileSync('./result1', 'utf8');
+    const result2 = fs.readFileSync('./result2', 'utf8');
+    expect(result1).toBe(result2);
+  });
 });
